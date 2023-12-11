0e1ec7316b10b5451e97c93c71f1da2f02bff990
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-12-12 00:57:33 +0600

    added education section

--- src/Components/Education/Education.jsx
+++ src/Components/Education/Education.jsx
@@ -11,64 +11,98 @@ const Education = () => {
     <div id="education">
       <SectionTitle title={"Education Details"}></SectionTitle>
 
-      <VerticalTimeline>
+      <VerticalTimeline lineColor={"#70FF00"}>
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          className="vertical-timeline-element--work "
+          contentStyle={{ backgroundColor: "transparent", color: "#828282" }}
+          contentArrowStyle={{ borderRight: "7px solid  #70FF00" }}
           date="2023 - present"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          dateClassName={"text-[#70FF00]"}
+          iconStyle={{ backgroundColor: "#161616", color: "#70FF00" }}
           icon={<MdCastForEducation />}
         >
-          <h3 className="vertical-timeline-element-title">Creative Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">Miami, FL</h4>
+          <h3 className="vertical-timeline-element-title text-[#70FF00]">
+            Frontend Developer
+          </h3>
+
           <p>
-            Creative Direction, User Experience, Visual Design, Project
-            Management, Team Leading
+            In 2023, I completed a web development course at Programming Hero.
+            Over the last six months, I devoted significant time to research,
+            enhancing my understanding of various technologies. This period has
+            been particularly fruitful for my learning journey. Additionally, I
+            successfully completed my ninth-grade studies during this time.
           </p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
+          className="vertical-timeline-element--work "
+          contentStyle={{ backgroundColor: "transparent", color: "#828282" }}
+          contentArrowStyle={{ borderRight: "7px solid  #70FF00" }}
           date="2022"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          dateClassName={"text-[#70FF00]"}
+          iconStyle={{ backgroundColor: "#161616", color: "#70FF00" }}
           icon={<MdCastForEducation />}
         >
-          <h3 className="vertical-timeline-element-title">Art Director</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
+          <h3 className="vertical-timeline-element-title text-[#70FF00]">
+            Self Learner
+          </h3>
+
           <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online
-            Marketing
+            In 2022, I embarked on a challenging solo journey into web
+            development. Progress was slow, but the experience proved
+            invaluable. The turning point came when I started the Programming
+            Hero course, where I found significant help and realized the
+            effectiveness of structured learning.
           </p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
+          className="vertical-timeline-element--work "
+          contentStyle={{ backgroundColor: "transparent", color: "#828282" }}
+          contentArrowStyle={{ borderRight: "7px solid  #70FF00" }}
           date="2020 - 2021"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          dateClassName={"text-[#70FF00]"}
+          iconStyle={{ backgroundColor: "#161616", color: "#70FF00" }}
           icon={<MdCastForEducation />}
         >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            Los Angeles, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
+          <h3 className="vertical-timeline-element-title text-[#70FF00]">
+            Story of my own business
+          </h3>
+
+          <p>
+            From 2020 to 2021, I endeavored to build an online e-commerce
+            business. However, while working on my own e-commerce site, I
+            realized my experience was limited. Recognizing the need for more
+            knowledge, I decided to pause the business. Despite the challenges,
+            the experience provided valuable insights that contribute to my
+            growth.
+          </p>
         </VerticalTimelineElement>
 
         <VerticalTimelineElement
-          className="vertical-timeline-element--work"
+          className="vertical-timeline-element--work "
+          contentStyle={{ backgroundColor: "transparent", color: "#828282" }}
+          contentArrowStyle={{ borderRight: "7px solid  #70FF00" }}
           date="2013 - 2019"
-          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          dateClassName={"text-[#70FF00]"}
+          iconStyle={{ backgroundColor: "#161616", color: "#70FF00" }}
           icon={<MdCastForEducation />}
         >
-          <h3 className="vertical-timeline-element-title">Web Designer</h3>
-          <h4 className="vertical-timeline-element-subtitle">
-            San Francisco, CA
-          </h4>
-          <p>User Experience, Visual Design</p>
+          <h3 className="vertical-timeline-element-title text-[#70FF00]">
+            My Studies at the Madrasah.
+          </h3>
+
+          <p>
+            From 2013 to 2019, I studied at a madrasah, where I earned my Hafez
+            degree. Additionally, I completed two sessions of the Mawlana
+            division.
+          </p>
         </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          iconStyle={{ backgroundColor: "#161616", color: "#70FF00" }}
+          icon={<MdCastForEducation />}
+        />
       </VerticalTimeline>
     </div>
   );
